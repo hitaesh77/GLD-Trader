c249a12b4187774b55f5f8a36926faf9103bdeda
koidebot <rkoide@andrew.cmu.edu>
2025-08-02 21:35:25 -0400

    model completion

--- data/ingestion/combine_data.py
+++ data/ingestion/combine_data.py
@@ -3,16 +3,18 @@ import pandas as pd
 def combine(df_gld, df_macro, df_news):
     df_gld = df_gld.copy()
     df_macro = df_macro.copy()
-    df_news = df_news.copy()
+    # df_news = df_news.copy()
 
-    df = pd.concat([df_gld, df_macro, df_news], axis=1, join='inner', ignore_index=False)
-    df.drop(columns=["Dividends","Stock Splits", "Capital Gains", "headlines", "currency", "headlines", "sentiments"], inplace=True, errors='ignore')
+    # df = pd.concat([df_gld, df_macro, df_news], axis=1, join='inner', ignore_index=False)
+    df = pd.concat([df_gld, df_macro], axis=1, join='inner', ignore_index=False)
+    df.drop(columns=["Dividends","Stock Splits", "Capital Gains", "currency"], inplace=True, errors='ignore')
     return df
 
 if __name__ == "__main__":
     df_gld = pd.read_csv("data/gld_daily.csv", index_col='date', parse_dates=True)
-    df_macro = pd.read_csv("data/finbert_gold_news.csv", index_col='date', parse_dates=True)
-    df_news = pd.read_csv("data/macro_data.csv", index_col='date', parse_dates=True)
+    # df_news = pd.read_csv("data/finbert_gold_news.csv", index_col='date', parse_dates=True)
+    df_macro = pd.read_csv("data/macro_data.csv", index_col='date', parse_dates=True)
 
-    combined_df = combine(df_gld, df_macro, df_news)
+
+    combined_df = combine(df_gld, df_macro, None)
     combined_df.to_csv("data/combined_data.csv")
